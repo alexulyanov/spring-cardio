26c7c75b51e338fb3e5a5f86772fffd4a86829bd
Alex <alex.ulyanov90@gmail.com>
2019-04-07 15:12:06 -0400

    initial commit

---
+++ src/test/java/com/clinic/cardio/CardioApplicationTests.java
@@ -0,0 +1,16 @@
+package com.clinic.cardio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CardioApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
