03e0f9ce36b36dea5ac2223b66e69de9b3efe00f
Aleksei Ulianov <aulianov@starstechnologies.com>
2020-06-07 16:16:33 -0400

    Fixed enum issues. Fixed broken templates.

--- src/main/java/com/clinic/cardio/controllers/PatientRestController.java
+++ src/main/java/com/clinic/cardio/controllers/PatientRestController.java
@@ -4,11 +4,13 @@ import com.clinic.cardio.models.Patient;
 import com.clinic.cardio.repositories.PatientRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class PatientRestController {
 
     private final PatientRepository patientRepository;
@@ -17,7 +19,7 @@ public class PatientRestController {
         this.patientRepository = cardioService;
     }
 
-    @GetMapping(path="/api/patients")
+    @GetMapping(path="/patients")
     public ResponseEntity<?> getAllEmployees(){
         List<Patient> result = patientRepository.findAll();
         return ResponseEntity.ok(result);
